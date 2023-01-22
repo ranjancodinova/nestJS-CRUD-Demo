b3b981eea053245a5e4b18e44f8a13ed92bc2c88
Ranjan sharma <ranjan.sharma@codinova.com>
2023-01-22 09:12:07 +0530

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Hotel Booking')
+    .setDescription('This site provide feature of booking online hotel with best filter & price')
+    .setVersion('1.0')
+    .addTag('hotel')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
